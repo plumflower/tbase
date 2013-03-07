66d3e61fad621cbf73a617ee7a7d8e81ebd70bb1
U-sunday-PC\sunday <sunday@sunday-PC.(none)>
2013-03-07 21:36:15 +0800

    add tbase project

---
+++ tbase/tbase-config.h
@@ -0,0 +1,13 @@
+/*
+  * tbase-config.h
+  *
+  * Date: 2013-3-7
+  * Author: sundayliu
+  */
+
+#ifndef TBASE_CONFIG_H
+#define TBASE_CONFIG_H
+
+#define FILEBUFF 8192
+
+#endif // end of TBASE_CONFIG_H
